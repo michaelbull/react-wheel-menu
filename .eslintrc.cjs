94abad42e7724b06590cf17c00839b1560dbd83d
Michael Bull <mikebull94@gmail.com>
2023-07-01 17:51:41 +0100

    Make eslintrc a commonjs module
    See: https://github.com/eslint/eslint/issues/14137

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -2,35 +2,33 @@
  * @type {import('@types/eslint').Linter.BaseConfig}
  */
 module.exports = {
-    parser: '@typescript-eslint/parser',
+    env: {
+        browser: true
+    },
     extends: [
         'plugin:@typescript-eslint/recommended',
-        'plugin:react/recommended'
+        'plugin:react/recommended',
+        'plugin:react/jsx-runtime'
     ],
+    parser: '@typescript-eslint/parser',
+    parserOptions: {
+        ecmaVersion: 2020,
+        sourceType: 'module'
+    },
     plugins: [
         '@typescript-eslint',
         'react',
         'react-hooks'
     ],
-    settings: {
-        react: {
-            version: 'detect'
-        }
-    },
-    env: {
-        browser: true
-    },
-    parserOptions: {
-        ecmaVersion: 2018,
-        sourceType: 'module',
-        ecmaFeatures: {
-            jsx: true
-        }
-    },
     rules: {
         'react/jsx-uses-vars': 'warn',
         'react/jsx-uses-react': 'warn',
         'react-hooks/rules-of-hooks': 'error',
         'react-hooks/exhaustive-deps': 'error'
+    },
+    settings: {
+        react: {
+            version: 'detect'
+        }
     }
 };
